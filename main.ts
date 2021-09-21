57dbd2d6933da4abd3dc8002f7be3d69fc1046cd
Richard Hristov <richardhristov@gmail.com>
2021-09-21 21:24:37 +0300

    Set default number of workers to 1

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 import path from "path";
 import { startCluster } from "./utils/cluster";
 
-let workers = parseInt(process.env.NODE_WORKERS?.toString() ?? "2");
+let workers = parseInt(process.env.NODE_WORKERS?.toString() ?? "1");
 if (isNaN(workers) || workers < 1) {
-	workers = 2;
+	workers = 1;
 }
 
 startCluster(path.join(__dirname, "server.js"), workers);
